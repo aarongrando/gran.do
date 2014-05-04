5a4243616cd212b52a944b22a102730d81e48f68
Aaron Grando <aaron.grando@gmail.com>
2014-05-04 15:30:54 -0400

    starting the about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Grrrando::Application.routes.draw do
 
   root to: 'home#index'
 
-  get '/cv', to: 'home#cv', as: :cv
+  get '/cv',      to: 'home#cv',    as: :cv
+  get '/purpose', to: 'blog#about', as: :purpose
   
-  get '/blog/fall',                                             to: 'blog#fall',            as: :blog_fall
-  get '/blog/finishing',                                        to: 'blog#finishing',       as: :blog_finishing
-  get '/blog/lets-hang-out',                                    to: 'blog#hang_out',        as: :blog_hang_out
-  get '/blog/intro-to-heroku-addons',                           to: 'blog#heroku',          as: :blog_heroku
-  get '/blog/1-line-productivity-hack',                         to: 'blog#productivity',    as: :blog_productivity
-  get '/blog/engineers-and-advertising',                        to: 'blog#engineers',       as: :blog_engineers
-  get '/blog/complexity-equals-features-squared',               to: 'blog#complexity',      as: :blog_complexity
-  get '/blog/what-ad-agencies-job-openings-are-really-saying',  to: 'blog#job_openings',    as: :blog_job_openings
+  get '/blog/fall',                                             to: 'blog#fall',          as: :blog_fall
+  get '/blog/finishing',                                        to: 'blog#finishing',     as: :blog_finishing
+  get '/blog/this-website',                                     to: 'blog#about',         as: :blog_about
+  get '/blog/lets-hang-out',                                    to: 'blog#hang_out',      as: :blog_hang_out
+  get '/blog/intro-to-heroku-addons',                           to: 'blog#heroku',        as: :blog_heroku
+  get '/blog/1-line-productivity-hack',                         to: 'blog#productivity',  as: :blog_productivity
+  get '/blog/engineers-and-advertising',                        to: 'blog#engineers',     as: :blog_engineers
+  get '/blog/complexity-equals-features-squared',               to: 'blog#complexity',    as: :blog_complexity
+  get '/blog/what-ad-agencies-job-openings-are-really-saying',  to: 'blog#job_openings',  as: :blog_job_openings
+
+
 
 end
