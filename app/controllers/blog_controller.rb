7da3f7e6e9e7ebde62e9620e5917f45ad3c2526d
Aaron Grando <aaron.grando@gmail.com>
2014-05-15 15:46:59 -0400

    whoa heroku test

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,3 +1,10 @@
 class BlogController < ApplicationController
   
+  def whoa 
+    if !request.fullpath.include? 'whoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      @target_path = request.fullpath << 'a'
+      render partial: 'whoa_redirect', layout: false 
+    end
+  end
+  
 end
