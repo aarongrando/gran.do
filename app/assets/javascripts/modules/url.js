698b8b496666ebd0e0d09a37ce81d89d27f2bfa0
Aaron Grando <aaron.grando@gmail.com>
2014-05-20 14:28:23 -0400

    urls blog post

--- app/assets/javascripts/modules/url.js
+++ app/assets/javascripts/modules/url.js
@@ -1,19 +1,21 @@
 $.fn.Url = function() {
 
 	if ($(this).length > 0) {
-		history.pushState(null, null, '/blog/')
-		var whoa_interval = setInterval(growURL, 30),
-			url_step = 0,
-			url_to_build = "for-the-love-of-the-url"
+		var url_step = 0,
+			url_to_build = "for-the-love-of-the-url",
+			base_url_fragments = document.URL.split('/'),
+			base_url_path = '/' + base_url_fragments[base_url_fragments.length - 2] + '/'
+
+		history.pushState(null, null, base_url_path)
+		var whoa_interval = setInterval(growURL, 30)
 	}
 	
 	function growURL() {
 		if (_.last(document.URL.split('/')).length < url_to_build.length) {
-			history.replaceState(null, null, '/blog/' + url_to_build.substring(0,url_step))
+			history.replaceState(null, null, base_url_path + url_to_build.substring(0,url_step))
 			url_step++;
 		} else {
 			clearInterval(whoa_interval)
 		}
 	}
-	
 }
