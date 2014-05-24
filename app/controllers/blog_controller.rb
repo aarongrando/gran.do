bb88515b856c1e7a59ce9a3ae974c963c72b0b63
Aaron Grando <aaron.grando@gmail.com>
2014-05-24 16:19:23 -0400

    fix ga tracking to account for URL rewrites on URL post

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,10 +1,7 @@
 class BlogController < ApplicationController
   
   def whoa 
-    # if !request.fullpath.include? 'whoaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-#       @target_path = request.fullpath << 'a'
-#       render partial: 'whoa_redirect', layout: false 
-#     end
+    
   end
   
 end
