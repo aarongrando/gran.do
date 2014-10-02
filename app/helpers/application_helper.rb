85e0a61ed5b63e5ecf664b71fdc7d85edb6453d8
Aaron Grando <aaron.grando@gmail.com>
2014-10-02 10:42:59 -0400

    offline mode toggle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
   def controller_action
     "#{params[:controller]}##{params[:action]}"
   end
+
+  def offline_mode?
+    true # manual toggle for developing offline
+  end
   
 end
